5286f8909d9bf8cafe6bec37a559a4fa03dba624
Jeff Edwards <js_edwards@yahoo.com>
2013-11-19 15:30:08 -0800

    added words and event listeners

--- sightwords.js
+++ sightwords.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
-  
+  Sightwords.listenForClicks()
 })
+
+
+var Sightwords = {
+
+  listenForClicks: function(){
+    $('body').on('click','.word',function(){
+      event.preventDefault()
+      var currentWord = $(this).data("word")
+      console.log("you clicked on "+currentWord)
+    })
+  }
+}
